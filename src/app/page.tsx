12461460e4a771855e89863cb22c7748d4a1d42a
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-17 18:30:56 +0500

    navigation menu links hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="min-h-screen w-full flex items-end">
           <section className="w-full flex flex-row justify-between mb-32">
             <article className="w-[390px]">
-              <h1 className="uppercase text-2xl tracking-wider text-slate-100 text-spaceBlue">so, you want to travel to</h1>
+              <h1 className="uppercase text-2xl tracking-wider text-spaceBlue">so, you want to travel to</h1>
               <h1 className={`uppercase font-thin text-[8.3rem] leading-[10rem] mb-8 ${bellefair.className}`}>Space</h1>
               <p className=" text-spaceBlue">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </article>

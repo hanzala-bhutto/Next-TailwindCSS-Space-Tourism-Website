cb91744a94888327077e3797147520c8c7d840a6
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-20 20:57:26 +0500

    made tabs for destination planets

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,38 +1,70 @@
+'use client';
+
 import {barlowCond, bellefair} from "@/fonts/fonts";
-import Image from "next/image";
+import { useState } from "react";
+import {PlanetDescription} from "@/components";
+
+interface Planet {
+    index: number;
+    name: string;
+    description: string;
+    distance: string;
+    travelTime: string;
+    image: string;
+}
+
 
 export default function Destination() {
+
+    const [tab,setTab] = useState(0);
+
+    const planet = [
+        {
+            index: 0,
+            name: "Moon",
+            description: "cras aliquet varius magna, non porta ligula feugiat eget. fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.",
+            distance: "384,400 KM",
+            travelTime: "3 DAYS",
+            image: "/assets/destination/image-moon.png"
+        },
+        // mars description
+        {
+            index : 1,
+            name: "Mars",
+            description: "cras aliquet varius magna, non porta ligula feugiat eget. fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.",
+            distance: "225,300,000 KM",
+            travelTime: "9 MONTHS",
+            image: "/assets/destination/image-mars.png"
+        },
+        // Europa Object
+        {
+            index: 2,
+            name: "Europa",
+            description: "cras aliquet varius magna, non porta ligula feugiat eget. fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.",
+            distance: "628,300,000 KM",
+            travelTime: "3 YEARS",
+            image: "/assets/destination/image-europa.png"
+        },
+        // Titan Object
+        {
+            index: 3,
+            name: "Titan",
+            description: "cras aliquet varius magna, non porta ligula feugiat eget. fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.",
+            distance: "1,200,000,000 KM",
+            travelTime: "7 YEARS",
+            image: "/assets/destination/image-titan.png"
+        },
+      ]
+
+        const changeTab = (index: number) => {
+          setTab(index);
+        } 
+
     return (
         <div className="min-h-screen w-full flex flex-col justify-end gap-8">
           <h1 className={`uppercase text-3xl tracking-widest ${barlowCond.className}`}><span className="font-bold text-gray-600 mr-4">01</span> pick your destination</h1>
           <section className="w-full flex flex-row pl-20 justify-between items-center mb-14">
-            <article className="w-[400px] h-[300px] mb-10">
-              <Image src="/assets/destination/image-europa.png" alt="destination" width={400} height={300} />
-            </article>
-            <article className="w-[500px] flex flex-col gap-4">
-
-              <ul className={`flex flex-row gap-8 uppercase tracking-widest ${barlowCond.className} text-lg`}>
-                <li className="py-1  text-white border-b-4">Moon</li>
-                <li className="py-1  text-spaceBlue ">Mars</li>
-                <li className="py-1  text-spaceBlue ">Europa</li>
-                <li className="py-1  text-spaceBlue ">Titan</li>
-              </ul>
-
-              <h2 className={`text-8xl uppercase font-bold ${bellefair.className}`}>Moon</h2>
-              <p className="text-spaceBlue mb-4">cras aliquet varius magna, non porta ligula feugiat eget. fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. </p>
-              <hr className=" border-gray-600 mb-4"/>
-              <div className="flex flex-row gap-20">
-                <div className="flex flex-col">
-                    <h3 className="text-spaceBlue uppercase text-sm">AVG. DISTANCE</h3>
-                    <p className="text-3xl">384,400 KM</p>
-                </div>
-                <div className="flex flex-col">
-                    <h3 className="text-spaceBlue uppercase text-sm">EST. TRAVEL TIME</h3>
-                    <p className="text-3xl">3 DAYS</p>
-                </div>
-
-              </div>
-            </article>
+            <PlanetDescription planet={planet[tab]} changeTab={changeTab}/>
           </section>
         </div>
     )
---
+++ src/components/destination/PlanetDescription.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import {barlowCond, bellefair} from "@/fonts/fonts";
+
+
+interface Planet {
+    index: number;
+    name: string;
+    description: string;
+    distance: string;
+    travelTime: string;
+    image: string;
+}
+
+interface PlanetProps {
+    planet: Planet;
+    changeTab: (index: number) => void;
+}
+
+export const PlanetDescription : React.FC<PlanetProps> = ({planet, changeTab}) => {
+
+    return (
+        <>
+            <article className="w-[400px] h-[300px] mb-10">
+              <Image src={planet.image} alt="destination" width={400} height={300} />
+            </article>
+            <article className="w-[500px] flex flex-col gap-4">
+
+              <ul className={`flex flex-row gap-8 uppercase tracking-widest ${barlowCond.className} text-lg`}>
+                <li onClick={()=>changeTab(0)} className={`py-1 cursor-pointer hover:border-b-4 ${planet.index==0?'text-white border-b-4':'text-spaceBlue'}`}>Moon</li>
+                <li onClick={()=>changeTab(1)} className={`py-1 cursor-pointer hover:border-b-4 ${planet.index==1?'text-white border-b-4':'text-spaceBlue'}`}>Mars</li>
+                <li onClick={()=>changeTab(2)} className={`py-1 cursor-pointer hover:border-b-4 ${planet.index==2?'text-white border-b-4':'text-spaceBlue'}`}>Europa</li>
+                <li onClick={()=>changeTab(3)} className={`py-1 cursor-pointer hover:border-b-4 ${planet.index==3?'text-white border-b-4':'text-spaceBlue'}`}>Titan</li>
+              </ul>
+
+              <h2 className={`text-8xl uppercase font-bold ${bellefair.className}`}>{planet.name}</h2>
+              <p className="text-spaceBlue mb-4">{planet.description}</p>
+              <hr className=" border-gray-600 mb-4"/>
+              <div className="flex flex-row gap-20">
+                <div className="flex flex-col">
+                    <h3 className="text-spaceBlue uppercase text-sm">AVG. DISTANCE</h3>
+                    <p className="text-3xl">{planet.distance}</p>
+                </div>
+                <div className="flex flex-col">
+                    <h3 className="text-spaceBlue uppercase text-sm">EST. TRAVEL TIME</h3>
+                    <p className="text-3xl">{planet.travelTime}</p>
+                </div>
+
+              </div>
+            </article>
+        </>
+    )
+
+}

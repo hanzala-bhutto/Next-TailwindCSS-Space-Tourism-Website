81c43e606c114045cf7fe4dd1c0d171825c13a93
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-22 16:58:36 +0500

    technology page numbering hover

--- src/components/technology/TechDescription.tsx
+++ src/components/technology/TechDescription.tsx
@@ -9,9 +9,9 @@ export const TechDescription : React.FC<TechProps> = ({technology, changeTab}) =
             <article className="w-[700px] h-[350px] flex flex-row justify-center items-center gap-20">
 
               <ul className={`flex flex-col gap-8 uppercase tracking-widest ${barlowCond.className} text-lg`}>
-                <li onClick={()=>changeTab(0)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 hover:border-white ${technology.index==0?'bg-white text-black':'text-spaceBlue'}`}>1</li>
-                <li onClick={()=>changeTab(1)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 border-spaceBlue ${technology.index==1?'bg-white text-black':'text-spaceBlue'}`}>2</li>
-                <li onClick={()=>changeTab(2)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 border-spaceBlue ${technology.index==2?'bg-white text-black':'text-spaceBlue'}`}>3</li>
+                <li onClick={()=>changeTab(0)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 border-spaceBlue hover:border-white ${technology.index==0?'bg-white text-black':'text-spaceBlue'}`}>1</li>
+                <li onClick={()=>changeTab(1)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 border-spaceBlue hover:border-white ${technology.index==1?'bg-white text-black':'text-spaceBlue'}`}>2</li>
+                <li onClick={()=>changeTab(2)} className={`px-7 py-4 text-4xl text-center rounded-full cursor-pointer border-2 border-spaceBlue hover:border-white ${technology.index==2?'bg-white text-black':'text-spaceBlue'}`}>3</li>
               </ul>
 
               <div className="">

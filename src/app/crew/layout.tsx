94656e9e0d9d916c163a5127e5f187395d78e88d
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-25 18:37:50 +0500

    crew page completed

--- src/app/crew/layout.tsx
+++ src/app/crew/layout.tsx
@@ -6,7 +6,7 @@ export default function CrewLayout({
 }) {
   return (
         <>
-        <main className='min-h-screen flex flex-col items-center justify-between pl-40 pr-48 bg-crew bg-cover text-white overflow-y-hidden'>
+        <main className='min-h-screen flex flex-col items-center justify-between pt-20 pl-10 pr-10 lg:pt-0 lg:pl-40 lg:pr-48 bg-crew bg-cover text-white overflow-y-hidden'>
           {children}
         </main>
         </>
--- src/components/crew/CrewDescription.tsx
+++ src/components/crew/CrewDescription.tsx
@@ -6,14 +6,14 @@ export const CrewDescription : React.FC<CrewProps> = ({crew, changeTab}) => {
 
     return (
         <>
-            <article className="w-[600px] h-full flex flex-col justify-center gap-4">
+            <article className="lg:w-[600px] h-full flex flex-col justify-center items-center lg:items-start gap-4">
 
-              <h2 className={`text-4xl uppercase font-bold text-spaceBlue ${bellefair.className}`}>{crew.designation}</h2>
-              <h2 className={`text-6xl uppercase font-bold ${bellefair.className}`}>{crew.name}</h2>
+              <h2 className={`text-xl md:text-4xl uppercase font-bold text-spaceBlue ${bellefair.className}`}>{crew.designation}</h2>
+              <h2 className={`text-2xl md:text-6xl uppercase font-bold ${bellefair.className}`}>{crew.name}</h2>
               <p className="text-spaceBlue mb-4">{crew.description}</p>
 
-            <div className="mt-auto">
-              <ul className={`flex flex-row gap-8 uppercase tracking-widest ${barlowCond.className} text-lg`}>
+            <div className="mt-10 lg:mt-auto order-first lg:order-none">
+              <ul className={`flex flex-row justify-center md:justify-start gap-8 uppercase tracking-widest ${barlowCond.className} text-lg`}>
                 <li onClick={()=>changeTab(0)} className={`py-[8px] px-[10px] rounded-full cursor-pointer hover:bg-gray-400 ${crew.index==0?'bg-white border-b-4':'bg-gray-600'}`}></li>
                 <li onClick={()=>changeTab(1)} className={`py-[8px] px-[10px] rounded-full cursor-pointer hover:bg-gray-400 ${crew.index==1?'bg-white border-b-4':'bg-gray-600'}`}></li>
                 <li onClick={()=>changeTab(2)} className={`py-[8px] px-[10px] rounded-full cursor-pointer hover:bg-gray-400 ${crew.index==2?'bg-white border-b-4':'bg-gray-600'}`}></li>
@@ -21,8 +21,8 @@ export const CrewDescription : React.FC<CrewProps> = ({crew, changeTab}) => {
             </div>
             </article>
 
-            <article className="w-[400px] h-[400px]">
-              <Image src={crew.image} alt="destination" width={400} height={300} />
+            <article className="lg:w-[400px] lg:h-[400px]">
+              <Image className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]" src={crew.image} alt="destination" width={400} height={300} />
             </article>
 
         </>

472f0a89fe70b6953f34d1e2dd8361e5f0dfa0b3
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-25 10:00:55 +0500

    homepage completed

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({
 }) {
   return (
         <>
-        <main className='min-h-screen flex flex-col items-center justify-between bg-homeMobile pl-2 pr-2 md:pl-40 md:pr-48 md:bg-home bg-cover text-white'>
+        <main className='min-h-screen flex flex-col items-center justify-between bg-homeMobile pl-2 pr-2 md:px-10 lg:pl-40 lg:pr-48 md:bg-home bg-cover text-white'>
           {children}
         </main>
         </>

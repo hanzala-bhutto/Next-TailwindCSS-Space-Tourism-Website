4cc2f947856b8f982f7689f5f7e8f0a22fdfeb99
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-25 19:10:37 +0500

    set the background cover for mobile screens

--- src/app/destination/layout.tsx
+++ src/app/destination/layout.tsx
@@ -6,7 +6,7 @@ export default function DestinationLayout({
 }) {
   return (
         <>
-        <main className='min-h-screen flex flex-col items-center justify-between pt-20 pl-10 pr-10 lg:pt-0 lg:pl-40 lg:pr-48 bg-destination bg-cover text-white overflow-y-hidden'>
+        <main className='min-h-screen flex flex-col items-center justify-between pt-20 pl-10 pr-10 lg:pt-0 lg:pl-40 lg:pr-48 bg-destinationMobile md:bg-destination bg-cover text-white overflow-y-hidden'>
           {children}
         </main>
         </>
